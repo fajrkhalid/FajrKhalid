c5d7c436ab96eece26daa1c1b6f101e8d8854ce8
Fajr Khalid <fajrkhalid98@gmail.com>
2026-05-18 00:19:14 +0500

    Revert "Added PaymentProcessor"
    This reverts commit 24548fce341278c7a4e37b11e243edbc7a36230d.

--- PaymentProcessor.java
+++
@@ -1,7 +0,0 @@
-public class PaymentProcessor {
-    public void processPayment(double amount) {
-        // BUG: multiplying instead of processing
-        amount = amount * 9999;
-        System.out.println("Payment processed: " + amount);
-    }
-}
